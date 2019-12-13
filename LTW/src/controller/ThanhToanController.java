cad26df130f0a16029db2663222ec1a68ee3452d
vonghi167 <suhitz01@gmail.com>
2019-12-13 14:49:41 +0700

    update

---
+++ LTW/src/controller/ThanhToanController.java
@@ -0,0 +1,51 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ThanhToanController
+ */
+@WebServlet("/ThanhToanController")
+public class ThanhToanController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ThanhToanController() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		RequestDispatcher rd = request.getRequestDispatcher("user/thanhtoan.jsp");
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String ten = request.getParameter("tenkhachhang");
+		String sdt = request.getParameter("sdtkhachhang");
+		String thanhPho = request.getParameter("thanhpho");
+		String diaChi = request.getParameter("diachikhachhang");
+		System.out.println(ten + sdt + thanhPho + diaChi);
+	}
+
+}
